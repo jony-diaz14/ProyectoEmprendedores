fed0a5b8b9e80c7dcfb828b73371902d466845f6
Marlon Ángel <marlon.axel58@gmail.com>
2022-10-21 00:16:26 -0500

    footer and nav separados

---
+++ resources/views/layouts/navbar/navbar.blade.php
@@ -0,0 +1,22 @@
+<div class="container-fluid d-flex align-items-center justify-content-between">
+
+    <a href="" class="logo d-flex align-items-center scrollto me-auto me-lg-0">
+        <!-- Uncomment the line below if you also wish to use an image logo -->
+        <!-- <img src="{{ asset('') }}/img/logo.png" alt=""> -->
+        <h1>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;TravelConcierge<span>.</span></h1>
+    </a>
+
+    <nav id="navbar" class="navbar">
+        <ul>
+
+            <li><a class="nav-link scrollto" href="#propuesta">Propuesta</a></li>
+            <li><a class="nav-link scrollto" href="#price">Precios</a></li>
+            <li><a class="nav-link scrollto" href="#clientes">Clientes</a></li>
+            <li><a class="nav-link scrollto" href="#contactos">Contactos</a></li>
+
+    </nav>
+    <!-- .navbar -->
+
+    <a class="btn-getstarted scrollto" href="#">Ingreso</a>
+
+</div>
